87e126384500f3ddbe6e92bb51d08110cf4ce99f
canice james <ecanicthedeveloper@gmail.com>
2019-03-26 18:06:17 -0400

    build site

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Team B\'s web server' });
+  res.render('index', { title: 'Express' });
+});
+
+/* GET home page. */
+router.get('/canice', function(req, res, next) {
+  res.render('canice',);
+});
+
+/* GET home page. */
+router.get('/shafferson', function(req, res, next) {
+  res.render('shafferson', { title: 'Express' });
 });
 
 module.exports = router;
